5236151b1d5e72d44dbffce28be1fc9da7e54f1b
Chris Jeon <cjeon97@gmail.com>
2020-08-09 00:54:40 -0400

    add space

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
         else {
             callback(undefined, body.current.weather_descriptions[0] + ". It is currently " +
                 body.current.temperature + " degrees out and it feels like " +
-                body.current.feelslike + " degrees out." + "The humidity is currently " + body.current.humidity + "%.")
+                body.current.feelslike + " degrees out. " + "The humidity is currently " + body.current.humidity + "%.")
         }
     })
 }
