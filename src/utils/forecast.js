884bc97c2509b724776dff2f07a3441080b2b69c
Chris Jeon <cjeon97@gmail.com>
2020-08-08 23:53:36 -0400

    initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7233d6330bb3467302310489993413f2&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to location services.", undefined)
+        }
+        else if (body.error) {
+            callback("Unable to find location.", undefined)
+        }
+        else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " +
+                body.current.temperature + " degrees out. It feels like " +
+                body.current.feelslike + " degrees out.")
+        }
+    })
+}
+
+module.exports = forecast
